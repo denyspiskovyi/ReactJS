05b2abbd2319c80835d1c4402241f7a5f11741a5
denyspiskovyi <denys.piskovyi@gmail.com>
2024-06-29 13:21:18 +0300

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREz5MjAYgjIBAqmZkNB3KNOM5gyOmwWGcKTA&s'>
+        </img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+
+            <div>
+                <img src='https://user-images.githubusercontent.com/3616980/44358570-afc8ac00-a4b5-11e8-8f7e-1ad9b7038ce5.png'></img>
+            </div>
+
+            <div>
+                ava + description
+            </div>
+
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+
+                <div>
+                    Post 1
+                </div>
+
+                <div>
+                    Post 2
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
