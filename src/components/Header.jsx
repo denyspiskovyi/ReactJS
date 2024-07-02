e66149a3fa2cd77634aa1f53de11aabaa9162b97
denyspiskovyi <denys.piskovyi@gmail.com>
2024-07-02 15:20:59 +0300

    lesson14CssModule

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import h from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={h.header}>
         
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREz5MjAYgjIBAqmZkNB3KNOM5gyOmwWGcKTA&s'>
         </img>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
+import p from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={p.content}>
 
             <div>
                 <img src='https://user-images.githubusercontent.com/3616980/44358570-afc8ac00-a4b5-11e8-8f7e-1ad9b7038ce5.png'></img>
             </div>
 
-            <div>
+            <div className={p.ava}>
                 ava + description
             </div>
 
-            <div>
+            <div className={p.myPosts}>
                 My posts
-                <div>
+                <div className={p.newPost}>
                     New posts
                 </div>
 
-                <div>
+                <div className={p.post1}>
                     Post 1
                 </div>
 
-                <div>
+                <div className={p.post2}>
                     Post 2
                 </div>
                 
